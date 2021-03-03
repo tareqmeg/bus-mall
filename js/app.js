431afaa45f7520120a0c9ba1ffc35e3bd5d3d487
tareqmeg <tareq.megdadi@gmail.com>
2021-03-03 15:34:52 +0200

    using while insted of if condition

--- js/app.js
+++ js/app.js
@@ -164,7 +164,7 @@ function randomNumber( min, max ) {
   let theNumber = Math.floor( Math.random() * ( max - min + 1 ) ) + min;
 
   for( let i = 0;i < previousIndex.length;i++ ){
-    if ( theNumber === previousIndex[i] ){
+    while ( theNumber === previousIndex[i] ){
       theNumber = Math.floor( Math.random() * ( max - min + 1 ) ) + min;
     }
   }return( theNumber );
